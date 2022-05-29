dcd6a58050cf01dedf45d80846981d982fe819e8
Yaroslav Maneliuk <yaroslav.manelyuk@gmail.com>
2022-05-29 17:11:34 +0300

    Add class RangeValidator

--- index.js
+++ index.js
@@ -1,5 +1,41 @@
 "use strict";
 
+// Создать класс RangeValidator, со следующими свойствами:
+// ■ from (number);
+// ■ to (number);
+// Реализовать getter'ы и setter'ы для обоих свойств
+// Реализовать getter range, который будет возвращать массив с двумя числами диапазона
+// Реализовать метод validate, который будет принимать число и проверять входит ли число в указанный диапазон. Метод возвращает значение, если значение валидно. И кинет ошибку, если нет.
+
+class RangeValidator {
+  constructor(from, to) {
+    this._from = from;
+    rhis._to = to;
+  }
+  get from() {
+    return this._from;
+  }
+  set from(from) {
+    this._from = from;
+  }
+  get to() {
+    return this._to;
+  }
+  set to(to) {
+    this._to = to;
+  }
+  get range(){
+    return [this._from, this._to]
+  }
+  validate(number) {
+    if (this._from < this._to && number < this._to && number > this._from) {
+      return number;
+    } else {
+      throw RangeError("Number is out of range");
+    }
+  }
+}
+
 // Создать класс Figure3D
 // Создать классы-потомки для Шара, Цилиндра и Куба.
 // У всех классов должен быть метод для рассчета объема.
